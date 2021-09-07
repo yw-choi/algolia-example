46a5514fdc7b31c651a16c92c1cdaf052d6d78c2
Young Woo Choi <hi.ywchoi@gmail.com>
2021-09-07 03:03:44 -0700

    algolia example

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,84 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+const searchBoxClassNames = {
+  "ais-SearchBox": "",
+  "ais-SearchBox-form": "",
+  "ais-SearchBox-input": "",
+};
+</script>
 <template>
-  <nav class="flex flex-row justify-end w-full p-4">
-    <a href="https://github.com/yw-choi/instamade" class="" target="_blank">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-      >
-        <path
-          d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-        />
-      </svg>
-    </a>
-  </nav>
-  <header class="w-full flex flex-row justify-center items-center h-48">
-    <img src="./assets/logo.svg" alt="instamade" class="h-24" />
-  </header>
-  <section class="w-full flex flex-row justify-center items-center h-24">
-    <div class="flex items-center p-4 shadow-lg" style="width: 584px">
-      <div class="mr-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+  <header class="w-full bg-blue-500">
+    <nav class="flex flex-row justify-between w-full px-6 py-2">
+      <div class="flex">
+        <a href="#" class="flex items-center">
+          <img src="./assets/logo.svg" alt="instamade" class="h-8 mr-3" />
+          <span class="font-bold text-xl text-white">instamade</span>
+        </a>
       </div>
-      <input
-        class="w-full text-gray-800 leading-tight focus:outline-none"
-        type="text"
-        placeholder="Search"
-      />
+      <a href="https://github.com/yw-choi/instamade" target="_blank">
+        <img
+          src="./assets/GitHub-Mark-Light-64px.png"
+          alt="GitHub"
+          class="h-6 mt-1"
+        />
+      </a>
+    </nav>
+  </header>
+
+  <ais-instant-search
+    :search-client="searchClient"
+    index-name="instant_search"
+    class="w-full p-6"
+  >
+    <div class="w-full flex flex-row justify-between">
+      <div class="w-1/6">
+        <ais-refinement-list attribute="brand" />
+      </div>
+
+      <div class="w-5/6 flex flex-col items-stretch">
+        <div class="w-full">
+          <ais-search-box
+            placeholder="Search..."
+            submit-title="Submit"
+            reset-title="Reset"
+            autofocus
+            :class-names="searchBoxClassNames"
+          >
+            <template v-slot:submit-icon></template>
+            <template v-slot:reset-icon></template>
+          </ais-search-box>
+        </div>
+        <ais-hits>
+          <template v-slot:item="{ item }">
+            <article>
+              <h1>
+                <ais-highlight :hit="item" attribute="name" />
+              </h1>
+            </article>
+          </template>
+        </ais-hits>
+
+        <div class="pagination">
+          <ais-pagination />
+        </div>
+      </div>
     </div>
-  </section>
-  <section class="w-full"></section>
+  </ais-instant-search>
 </template>
+
+<script lang="ts">
+import algoliasearch from "algoliasearch/lite";
+
+export default {
+  data() {
+    return {
+      searchClient: algoliasearch(
+        "latency",
+        "6be0576ff61c053d5f9a3225e2a90f76"
+      ),
+    };
+  },
+};
+</script>
 
 <style>
 #app {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import InstantSearch from "vue-instantsearch/vue3/es";
 
-createApp(App).mount("#app");
+createApp(App).use(InstantSearch).mount("#app");
+// createApp(App).mount("#app");
